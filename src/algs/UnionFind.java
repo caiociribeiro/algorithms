4e57b1690d246e52732b247f0a7b97663bc57b49
Caio Ciriaco <caiociriacorib@gmail.com>
2025-11-10 11:48:43 -0300

    add union-find with path compression

---
+++ src/algs/UnionFind.java
@@ -0,0 +1,48 @@
+package algs;
+
+public class UnionFind {
+    private int[] parent;
+    private byte[] rank;
+    private int count;
+
+    public UnionFind(int n) {
+        if (n < 0) throw new IllegalArgumentException();
+        count = n;
+        parent = new int[n];
+        rank = new byte[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public int find(int p) {
+        if (p < 0 || p >= parent.length) throw new IllegalArgumentException();
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]]; // path compression
+            p = parent[p];
+        }
+        return p;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) return;
+        if (rank[rootP] < rank[rootQ]) parent[rootP] = rootQ;
+        else if (rank[rootQ] < rank[rootP]) parent[rootQ] = rootP;
+        else {
+            parent[rootQ] = rootP;
+            rank[rootP]++;
+        }
+        count--;
+    }
+}
